64a485003b2b53f2e7550c231d098ac3ed202b85
Gabriel Cabral <gabrielcabral@MacBook-Air.local>
2019-10-25 21:49:45 -0300

    knight crud complete

---
+++ controllers/knightController.js
@@ -0,0 +1,50 @@
+const { Knight } = require("../models/Knight")
+module.exports = {
+    getAll: ({ res }) => {
+        Knight.find({}, function (err, knights) {
+            if (err) return res.send(err)
+
+            let knightsList = []
+
+            knights.map(knight => {
+                let tempKnight = {
+                    Nome: knight.name,
+                    Idade: knight.age,
+                    Armas: knight.weapons.length,
+                    Atributo: knight.keyAttribute,
+                    Ataque: knight.attack,
+                    Exp: knight.exp
+                }
+                knightsList.push(tempKnight)
+            })
+            return res.send(knightsList)
+        })
+    },
+    saveKnight: ({ body, res }) => {
+        var knight = new Knight(body)
+
+        knight.save(function (err) {
+            if (err) return res.status(400).send(err)
+            res.send(knight);
+        })
+    },
+    getById: ({ params, res }) => {
+        Knight.findById(params.knightId, function (err, knight) {
+            if (err) return res.status(400).send(err)
+            res.send(knight)
+        })
+    },
+    deleteById: ({ params, res }) => {
+        Knight.deleteOne({ _id: params.knightId }, function (err, knight) {
+            if (err) return res.status(400).send(err)
+            res.send(knight)
+        })
+    },
+    updateKnight: ({ params, res, body }) => {
+        if (!body.nickname) res.status(400).send({ error: "Missing nickname property" })
+        Knight.findOneAndUpdate({ _id: params.knightId }, { nickname: body.nickname }, function (err, knight) {
+            if (err) return res.status(400).send(err)
+            res.send(knight)
+        })
+    }
+}
--- routes/knights.js
+++ routes/knights.js
@@ -1,21 +1,15 @@
-var express = require('express');
-var router = express.Router();
-const { Knight } = require("../models/Knight")
+const express = require('express');
+const router = express.Router();
+const { getAll, saveKnight, getById, deleteById, updateKnight } = require("../controllers/knightController")
 
-router.post('/', function (req, res, next) {
-  var knight = new Knight(req.body)
-  knight.save(function (err) {
-    if (err) return res.send(err)
-    res.send(knight);
-  })
-});
+router.route('/')
+  .get(getAll)
+  .post(saveKnight)
 
-router.get('/', function (req, res, next) {
-  Knight.find({}).exec(function (err, response) {
-    if (err) return res.send(err)
-    return res.send(response)
-  })
-});
-
+  router.route('/:knightId')
+  .get(getById)
+  .delete(deleteById)
+  .put(updateKnight)
+ 
 
 module.exports = router;
