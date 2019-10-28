4419053bd947f3e6b7cf5de2951ff235c4bfc7e9
Gabriel Cabral <gabrielcabral@MacBook-Air.local>
2019-10-28 12:12:28 -0300

    knight model fix and add weapon model

--- views/knights-ui/src/_models/knight.model.ts
+++ views/knights-ui/src/_models/knight.model.ts
@@ -1,12 +1,15 @@
+import { Weapon } from './weapon.model';
+
 export class Knight {
     public name: string;
     public nickname: string;
     public birthday: Date;
-    public weapons: Array<Object>;
+    public weapons: Array<Weapon>;
     public attributes: Object;
     public keyAttribute: string;
 
     constructor(name: string) {
         this.name = name
+        this.attributes = {}
     }
 }
