a3d15114d98f9fadc4442fbb1e91c8d8bddf4730
Gabriel Cabral <gabrielcabral@MacBook-Air.local>
2019-10-29 01:53:21 -0300

    snackbar messages on http errors

--- views/knights-ui/src/app/list-knight/list-knight.component.ts
+++ views/knights-ui/src/app/list-knight/list-knight.component.ts
@@ -24,24 +24,19 @@ export class ListKnightComponent implements OnInit {
       this.isLoading = false
     }, error => {
       this.isLoading = false
-      // TODO: SNACKBAR INFORMANDO ERRO
+      this._snackBar.open("Não foi possivel obter a lista de Knights", 'OK', { duration: 2500 })
     })
   }
 
   deleteKnight(id: string): void {
     let knightToBeDeleted = this.knightsList.filter(knight => knight.id == id)
     knightToBeDeleted = knightToBeDeleted[0]
-    console.log("To be deleted", knightToBeDeleted);
-
     this.knightService.deleteKnight(id).subscribe(response => {
       this.knightsList.splice(this.knightsList.indexOf(knightToBeDeleted), 1)
-      console.log('After deletion', this.knightsList);
-      this._snackBar.open("Knight deletado com sucesso", '',  { duration: 2000 })
+      this._snackBar.open("Knight deletado com sucesso", 'OK', { duration: 2500 })
 
     }, error => {
-      console.log("Náo foi possivel deleter o knight");
-      // TODO: SNACKBAR INFORMANDO ERRO
-
+      this._snackBar.open("Náo foi possivel deleter o knight", 'OK', { duration: 2500 })
     })
   }
 
