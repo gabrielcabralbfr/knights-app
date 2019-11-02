5913815fa0bcd34457ed18eaa5a8889197c09113
Gabriel Cabral <gabrielcabral@MacBook-Air.local>
2019-11-01 22:05:42 -0300

    vue bootstrapping

---
+++ views/knights-vue/knights-vue/src/views/List.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h1>Lista</h1>
+    <KnightsList v-bind:knights="lista" msg="Welcome to Your List" />
+  </div>
+</template>
+
+<script>
+
+
+// @ is an alias to /src
+import KnightsList from '@/components/KnightList.vue'
+
+export default {
+  name: 'knightlist',
+  components: {
+    KnightsList
+  },
+  data: function lista(){
+      return {
+          lista: [{id: 1, name: 'Gabriel'}, {id: 2, name: 'Gabriel 2'}]}
+}}
+// this. lista =  [{id: 1, name: 'Gabriel'}]
+</script>
