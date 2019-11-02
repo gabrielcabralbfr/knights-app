f88aaca600dc032036ae470b2942bf01900a89c3
Gabriel Cabral <gabrielcabral@MacBook-Air.local>
2019-11-01 23:28:18 -0300

    basic routing

---
+++ views/knights-vue/knights-vue/src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-app-bar color="black accent-4" dark>
+      <v-toolbar-title>Knights Application</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <router-link to="/knight/list">
+          <v-icon>mdi-view-list</v-icon>
+        </router-link>
+      </v-btn>
+
+      <v-btn icon>
+        <router-link to="/knight/create">
+          <v-icon>mdi-plus-thick</v-icon>
+        </router-link>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<style lang="css" scoped>
+a:visited {
+  text-decoration: none;
+  color: #fff;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
+
---
+++ views/knights-vue/knights-vue/src/views/CreateKnight.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>Criacao</h1>
+    <!-- <KnightsList v-bind:knights="lista" msg="Welcome to Your List" /> -->
+  </div>
+</template>
+
+<script>
+
+
+// @ is an alias to /src
+// import KnightsList from '@/components/KnightList.vue'
+
+export default {
+  name: 'knightlist',
+  components: {
+    // KnightsList
+  },
+  data: function lista(){
+      return {
+          }
+}}
+</script>
