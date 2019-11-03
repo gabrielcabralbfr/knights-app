0e8b8e9cbdc0ee0613d9e3466e56055f0e87c433
Gabriel Cabral <gabrielcabral@MacBook-Air.local>
2019-11-03 02:23:18 -0300

    knight creation form

---
+++ views/knights-vue/knights-vue/src/components/KnightForm.vue
@@ -0,0 +1,181 @@
+<template>
+  <v-row class="d-flex justify-center mb-5" align="center">
+    <v-form ref="form" v-model="valid" :lazy-validation="false">
+      <v-stepper v-model="step">
+        <v-stepper-header>
+          <v-stepper-step @click="step = 1" :complete="step > 1" step="1">Guerreiro</v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step @click="step = 2" :complete="step > 2" step="2">Armas</v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step @click="step = 3" step="3">Atributos</v-stepper-step>
+        </v-stepper-header>
+
+        <v-stepper-items>
+          <v-stepper-content step="1">
+            <v-text-field
+              v-model="knight.name"
+              :rules="[v => !!v || 'Nome do guerreiro é obrigatório']"
+              label="Nome"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="knight.nickname"
+              :rules="[v => !!v || 'Apelido do guerreiro é obrigatório']"
+              label="Apelido"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="knight.keyAttribute"
+              :rules="[v => !!v || 'Atributo principal do guerreiro é obrigatório']"
+              label="Atributo Principal"
+              required
+            ></v-text-field>
+            <div class="mb-3 d-flex flex-column">
+              <p class="birthday pb-3 text-center">Data de Nascimento do Guerreiro</p>
+              <v-date-picker class="datepicker" v-model="knight.birthday" required></v-date-picker>
+            </div>
+            <v-btn color="default" class="float-right" @click="step = 2">Avançar</v-btn>
+          </v-stepper-content>
+
+          <v-stepper-content step="2">
+            <v-text-field
+              v-model="weapon.name"
+              :rules="[v => !!v || 'Nome da arma é obrigatório']"
+              label="Nome da Arma"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="weapon.attribute"
+              :rules="[v => !!v || 'Atributo da arma é obrigatório']"
+              label="Atributo da Arma"
+              required
+            ></v-text-field>
+            <v-switch color="primary" v-model="weapon.equipped" :label="`Equipada`"></v-switch>
+            <v-subheader class="pl-0 pb-3">Mod</v-subheader>
+            <v-slider v-model="weapon.mod" thumb-label="always"></v-slider>
+            <v-btn class="float-right" color="default" @click="step = 3">Avançar</v-btn>
+
+            <v-btn text @click="step = 1">Voltar</v-btn>
+          </v-stepper-content>
+
+          <v-stepper-content step="3">
+            <!-- <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card> -->
+            <div class="mt-4 grid-container">
+              <div>
+                <v-subheader class="pl-0 pb-3 d-block">Força</v-subheader>
+                <v-slider v-model="knight.attributes.strength" thumb-label="always" class="d-block"></v-slider>
+              </div>
+              <div>
+                <v-subheader class="pl-0 pb-3 d-block">Destreza</v-subheader>
+                <v-slider
+                  v-model="knight.attributes.dexterity"
+                  thumb-label="always"
+                  class="d-block"
+                ></v-slider>
+              </div>
+              <div>
+                <v-subheader class="pl-0 pb-3 d-block">Constituiçã0</v-subheader>
+                <v-slider
+                  v-model="knight.attributes.constituition"
+                  thumb-label="always"
+                  class="d-block"
+                ></v-slider>
+              </div>
+              <div>
+                <v-subheader class="pl-0 pb-3 d-block">Inteligência</v-subheader>
+                <v-slider
+                  v-model="knight.attributes.intelligence"
+                  thumb-label="always"
+                  class="d-block"
+                ></v-slider>
+              </div>
+              <div>
+                <v-subheader class="pl-0 pb-3 d-block">Sabedoria</v-subheader>
+                <v-slider v-model="knight.attributes.wisdom" thumb-label="always" class="d-block"></v-slider>
+              </div>
+              <div>
+                <v-subheader class="pl-0 pb-3 d-block">Carisma</v-subheader>
+                <v-slider v-model="knight.attributes.charisma" thumb-label="always" class="d-block"></v-slider>
+              </div>
+            </div>
+            <v-btn
+              :disabled="!valid"
+              color="primary"
+              class="mr-4 float-right"
+              @click="submitForm"
+            >Criar</v-btn>
+
+            <v-btn text @click="step = 2">Voltar</v-btn>
+          </v-stepper-content>
+        </v-stepper-items>
+      </v-stepper>
+    </v-form>
+  </v-row>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "KnightForm",
+  data() {
+    return {
+      valid: false,
+      step: 0,
+      knight: {
+        name: "",
+        nickname: "",
+        birthday: "",
+        weapons: [],
+        attributes: {},
+        keyAttribute: "",
+        id: ""
+      },
+      weapon: {
+        name: "",
+        mod: 0,
+        attribute: "",
+        equipped: false
+      }
+    };
+  },
+  methods: {
+    submitForm() {
+      this.knight.weapons.push(this.weapon);
+      console.log(this.knight);
+      axios.post("http://localhost:3000/knights", this.knight)
+      .then(response => console.log(response))
+      .catch(err => console.log(err))
+    },
+    validate() {}
+  }
+};
+</script>
+
+<style lang="css" scoped>
+form {
+  width: 80%;
+  max-width: 750px;
+  margin-top: 5%;
+}
+.v-input {
+  padding: 12px;
+}
+.grid-container {
+  display: grid;
+  grid-column-gap: 10%;
+  grid-template-columns: auto auto;
+}
+.datepicker {
+  margin: 0 auto;
+}
+.birthday {
+  color: rgba(0, 0, 0, 0.54);
+}
+.v-stepper:not(.v-stepper--vertical) .v-stepper__label {
+  display: block !important;
+}
+</style>
